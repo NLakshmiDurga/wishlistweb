cf769072925cf593a839e555958f5c6db3a558c9
NLakshmiDurga <lakshmidurganarumanchi@gmail.com>
2016-12-22 23:56:38 +0530

    Changes made in users.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,10 +5,18 @@
  * Date: 22/12/16
  * Time: 11:40 PM
  */
-    header('Access-Control-Allow-Origin:*');
-	$postdata = file_get_contents("php://input");
-    $request = json_decode($postdata);
-	echo "\nemailid : ",$request->email;
-	echo "\npassword : ",$request->pass;
-	// echo "<script> console.log('i am in php file')</script>";
+    class Login extends CI_Controller{
+        public function __construct()
+        {
+            parent::__construct();
+            // $this->load->library('session');
+        }
+        public function signin(){
+            header('Access-Control-Allow-Origin:*');
+            $postdata = file_get_contents("php://input");
+            $request = json_decode($postdata);
+            echo "\nemailid : ",$request->email;
+            echo "\npassword : ",$request->pass;
+        }
+    }
 ?>
